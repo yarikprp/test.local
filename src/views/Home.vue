291d8d4c34abc06f8af8a25c3078436ca5ba43a7
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-02-08 17:04:37 +0300

    Настроил переход к пользователем

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="card">
-    <h1>Vue</h1>
-    <button class="bth">Открыть пользователя</button>
+    <h1>{{ title }}</h1>
+    <button class="bth" @click="navigate">Открыть пользователя</button>
   </div>
 </template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { useRouter } from 'vue-router';
+
+  export default defineComponent( {
+    setup() {
+      const router = useRouter()
+      return {
+        title: 'Vue',
+        navigate: () => router.push('/users')
+      }
+    }
+  })
+</script>
